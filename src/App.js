fa6c71d47e7f8afe84db836922efe1be1946b03c
DarkElf2233 <fd.melnikoff@gmail.com>
2024-04-25 13:39:30 +0300

    Hide scroll bars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { ShopItem } from "./components/ShopItem";
+import styles from "./App.module.css";
 
 function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={styles['background-blue']}></div>
+      <div className={styles['background-white']}></div>
+      <div className={styles['content']}>
+        <div className={styles['item-image']}></div>
+        <div className={styles['window']}>
+          <ShopItem item={item} />
+        </div>
+      </div>
+    </>
   );
 }
 
